2303c5fe27642b1aadd8692a8a550d10ba9487b3
fritzgt <tuanis@live.com>
2020-09-09 13:02:30 +0300

    completed exersice

--- SwitchStatementsOBJC/SwitchStatementsOBJC/main.m
+++ SwitchStatementsOBJC/SwitchStatementsOBJC/main.m
@@ -5,13 +5,33 @@
 //  Created by FGT MAC on 9/9/20.
 //  Copyright © 2020 FGT MAC. All rights reserved.
 //
+enum AnimalNames {
+    Lion = 1,
+    Monkey = 2,
+    Tiger = 3,
+    Fish = 4
+};
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    
+    
+    int animalName = Tiger;
+    
+    switch (animalName) {
+        case Lion:
+            NSLog(@"This is a Lion.");
+            break;
+        case Monkey:
+            NSLog(@"This is a Monkey");
+        case Tiger:
+            NSLog(@"This is a Tiger");
+        default:
+            break;
     }
+    
+    
     return 0;
 }
